7cf9799e2dbcc2190b8613c19a71de89a1b868b7
Atul Kumar Khetan <atul.khetan@mtxb2b.com>
2020-04-05 23:16:44 +0530

    path url

--- django/user/urls.py
+++ django/user/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, re_path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path(
-        '', 
+        'login/', 
         LoginView.as_view(template_name='user/login.html'), 
-        name='login',
+        name='user_login',
     ),
+    path(
+        'logout/',
+        LogoutView.as_view(),
+        name='user_logout'
+    )
 ]
