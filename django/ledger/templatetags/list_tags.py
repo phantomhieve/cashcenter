3c7b09b5b5269256b1cade5aec81e589446ae779
Atul Kumar Khetan <atul.khetan@mtxb2b.com>
2020-05-28 01:35:41 +0530

    Ledger Done

--- django/ledger/templatetags/list_tags.py
+++ django/ledger/templatetags/list_tags.py
@@ -8,3 +8,12 @@ def url_replace(context, **kwargs):
     query = context['request'].GET.copy()
     query.update(kwargs)
     return query.urlencode()
+
+@register.simple_tag(takes_context=True)
+def get_shop(context, **kwargs):
+    user = context['request'].user
+    primary =  user.primary_user.filter().values('shop')
+    main    =  user.main_user.filter().values('shop')
+    if primary: return primary[0]['shop']
+    elif main: return f"{main[0]['shop']} - Admin"
+    return 'Super User'
--- django/user/models.py
+++ django/user/models.py
@@ -5,3 +5,7 @@ class UserGroup(models.Model):
     primary_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='primary_user', null=True)
     main_user    = models.ForeignKey(User, on_delete=models.CASCADE, related_name='main_user', null=True)
     shop         = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.primary_user}/{self.main_user}-\
+            {self.shop}"
