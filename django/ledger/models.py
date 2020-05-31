9a0a2da9ba1aa58c330099fad3ce94a3ca84996c
Atul Kumar Khetan <atul.khetan@mtxb2b.com>
2020-05-31 14:55:00 +0530

    Bug/user

--- django/ledger/models.py
+++ django/ledger/models.py
@@ -2,7 +2,7 @@ from djongo import models
 from django.contrib.auth.models import User
 
 class LedgerData(models.Model):
-    user         = models.OneToOneField(User, on_delete=models.CASCADE)
+    user         = models.ForeignKey(User, on_delete=models.CASCADE)
     l_r_no       = models.CharField(max_length=255)
     l_r_date     = models.DateField(blank=True, null=True)
     bale_no      = models.CharField(blank=True, null=True, max_length=255)
