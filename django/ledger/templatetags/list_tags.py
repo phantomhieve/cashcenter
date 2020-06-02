75aafed1ba06a4e3243c353db3fc556d5082435e
Atul Kumar Khetan <atul.khetan@mtxb2b.com>
2020-06-03 02:15:24 +0530

    page-fix/

--- django/ledger/templatetags/list_tags.py
+++ django/ledger/templatetags/list_tags.py
@@ -8,6 +8,7 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def url_replace(context, **kwargs):
     query = context['request'].GET.copy()
+    query.pop('page', None)
     query.update(kwargs)
     return query.urlencode()
 
