7cb383d2f1d8288bc0a23c98cf1efd07156035d5
Atul Kumar Khetan <atul.khetan@mtxb2b.com>
2020-06-03 02:09:21 +0530

    bug-fix

--- django/ledger/filters.py
+++ django/ledger/filters.py
@@ -50,24 +50,59 @@ class LedgerDataFilter(django_filters.FilterSet):
             'item':['contains']
         }
 
+    # Status 
+    STATUS_CHOICES = (
+        ('', 'All'),
+        ('True', 'Done'),
+        ('False', 'Pending')
+    )
+    status = django_filters.ChoiceFilter(
+        field_name='status',  
+        choices=STATUS_CHOICES, 
+        empty_label=None
+    )
+
+    # Extra field
+    DELIVERY_CHOICES = (
+        ('True', 'Delivered'),
+        ('False', 'Not-Delivered')
+    )
+    d_status = django_filters.ChoiceFilter(
+        label='d_status',
+        choices=DELIVERY_CHOICES,
+        method ='filter_by_delivery'
+    )
+    def filter_by_delivery(self, queryset, name, value):
+        print(value)
+        users = getUsersFromGroup(self.request.user)
+        queryset_ = queryset.filter(user__in=users)
+        if value=='True':
+            queryset_ = queryset_.exclude(delivery=None)
+        elif value=='False':
+            queryset_ = queryset_.filter(delivery=None)
+        return queryset_.values(*self.values)
+
+    # Extra Field
     columns = django_filters.MultipleChoiceFilter(
         label  = 'Columns',
         choices = CHOICES,
         method = 'filter_by_column',
     )
-
     def filter_by_column(self, queryset, name, value):
         value.append('id')
         self.values = value
         users = getUsersFromGroup(self.request.user)
-        return queryset.filter(user__in=users).values(*values)
+        return queryset.filter(user__in=users).values(*self.values)
 
+    # Extra Field
     l_r_date = django_filters.DateFromToRangeFilter(
         field_name='l_r_date',
         widget=django_filters.widgets.RangeWidget(
-            attrs={'type':'date', 'class': 'col-md-2 form-item-half'}
+            attrs={'type':'date'}
         )
     )
+
+    # Overriding Method
     @property
     def qs(self):
         queryset = super(LedgerDataFilter, self).qs
